f9f31d22ef280efd08fb79056bf482434aea4217
Reefact <sylvain.aurat@reefact.net>
2022-12-26 16:23:41 +0100

    Add chapter 7.5.3. examples.

---
+++ Reefact.BookExamples.Antlr4/Chapter_07/5/3/EvalListenerWithProps.cs
@@ -0,0 +1,56 @@
+﻿#region Usings declarations
+
+using Antlr4.Runtime.Tree;
+
+#endregion
+
+namespace Reefact.BookExamples.Antlr4.Chapter_07._5._3 {
+
+    public sealed class EvalListenerWithProps : LExprBaseListener {
+
+        #region Fields declarations
+
+        private readonly ParseTreeProperty<int> _values = new();
+        private          int                    _result;
+
+        #endregion
+
+        /// <inheritdoc />
+        public override void ExitInt(LExprParser.IntContext context) {
+            ITerminalNode integerNode      = context.INT();
+            string        integerAsString  = integerNode.GetText();
+            int           evaluatedInteger = int.Parse(integerAsString);
+
+            _values.Put(context, evaluatedInteger);
+        }
+
+        /// <inheritdoc />
+        public override void ExitAdd(LExprParser.AddContext context) {
+            int left              = _values.Get(context.e(0));
+            int right             = _values.Get(context.e(1));
+            int evaluatedAddition = left + right;
+
+            _values.Put(context, evaluatedAddition);
+        }
+
+        /// <inheritdoc />
+        public override void ExitMult(LExprParser.MultContext context) {
+            int left                    = _values.Get(context.e(0));
+            int right                   = _values.Get(context.e(1));
+            int evaluatedMultiplication = left * right;
+
+            _values.Put(context, evaluatedMultiplication);
+        }
+
+        /// <inheritdoc />
+        public override void ExitS(LExprParser.SContext context) {
+            _result = _values.Get(context.e());
+        }
+
+        public int Eval() {
+            return _result;
+        }
+
+    }
+
+}
---
+++ Reefact.BookExamples.Antlr4/Chapter_07/5/3/ReadMe.md
@@ -0,0 +1,12 @@
+﻿#### 7.5.3. Annotation Parse Trees
+
+~This example show the impact of rule options label on generated listeners.~
+
+_Remarks:_
+
+_C# lexer and parser classes are generated with the following command line:_
+
+```bat
+antlr4 SExpr.g4 -Dlanguage=CSharp
+antlr4 LExpr.g4 -Dlanguage=CSharp
+```
