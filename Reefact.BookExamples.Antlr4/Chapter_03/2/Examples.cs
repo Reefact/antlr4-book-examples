173f214e07ea51ae2679067192fa10b93d19840c
Reefact <sylvain.aurat@reefact.net>
2023-01-06 00:26:52 +0100

    Move code.

--- Reefact.BookExamples.Antlr4/Chapter_03/2/Examples.cs
+++ Reefact.BookExamples.Antlr4/Chapter_03/2/Examples.cs
@@ -19,7 +19,7 @@ namespace Reefact.BookExamples.Antlr4.Chapter_03._2 {
         [Fact]
         public void to_tokens_string() {
             // Setup
-            AntlrInputStream inputStream = AntlrInputStreamReader.Read("{99, 3, 451}\r\n");
+            AntlrInputStream inputStream = AntlrInputStreamReader.Read("{99, 3, 451}");
             GRun             grun        = GRun.Read(inputStream);
             // Exercise
             string tokens = grun.ToTokensString();
@@ -28,25 +28,25 @@ namespace Reefact.BookExamples.Antlr4.Chapter_03._2 {
         }
 
         [Fact]
-        public void convert_a_short_array_to_unicode_string() {
-            // Setup
-            AntlrInputStream inputStream = AntlrInputStreamReader.Read("{99, 3, 451}\r\n");
-            GRun             grun        = GRun.Read(inputStream);
-            // Exercise
-            string unicodeString = grun.ToUnicodeString();
-            // Verify
-            Check.That(unicodeString).IsEqualTo("\"\\u0063\\u0003\\u01C3\"");
-        }
-
-        [Fact]
         public void provide_a_lisp_tree_representation_of_a_short_array_in_ArrayInit_grammar() {
             // Setup
-            AntlrInputStream inputStream = AntlrInputStreamReader.Read("{99, 3, 451}\r\n");
+            AntlrInputStream inputStream = AntlrInputStreamReader.Read("{99, 3, 451}");
             GRun             grun        = GRun.Read(inputStream);
             // Exercise
             string lispStyleTree = grun.ToLispStyleTree();
             // Verify
             Check.That(lispStyleTree).IsEqualTo("(init { (value 99) , (value 3) , (value 451) })");
+        }
+
+        [Fact]
+        public void convert_a_short_array_to_unicode_string() {
+            // Setup
+            AntlrInputStream inputStream = AntlrInputStreamReader.Read("{99, 3, 451}");
+            GRun             grun        = GRun.Read(inputStream);
+            // Exercise
+            string unicodeString = grun.ToUnicodeString();
+            // Verify
+            Check.That(unicodeString).IsEqualTo("\"\\u0063\\u0003\\u01C3\"");
         }
 
         [Fact]
