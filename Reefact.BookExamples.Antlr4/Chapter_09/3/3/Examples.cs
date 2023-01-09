b73a4316662e27068ee68dade49459fe0d11d4c4
Reefact <sylvain.aurat@reefact.net>
2023-01-09 23:56:20 +0100

    Add chapter 9.3.3. single token deletion examples.

---
+++ Reefact.BookExamples.Antlr4/Chapter_09/3/3/Examples.cs
@@ -0,0 +1,43 @@
+﻿#region Usings declarations
+
+using Antlr4.Runtime;
+
+using ApprovalTests;
+using ApprovalTests.Reporters;
+
+using Reefact.BookExamples.Antlr4.Chapter_09._1;
+
+using Xunit;
+
+#endregion
+
+namespace Reefact.BookExamples.Antlr4.Chapter_09._3._3 {
+
+    [UseReporter(typeof(BeyondCompareReporter))]
+    public class Examples {
+
+        [Fact]
+        public void single_token_deletion_mermaid_style_tree() {
+            // Setup
+            AntlrInputStream inputStream = AntlrInputStreamReader.Read("class T {{ int i; }");
+            GRun             grun        = GRun.Read(inputStream);
+            // Exercise
+            string mermaidStyleTree = grun.ToMermaidStyleTree();
+            // Verify
+            Approvals.Verify(mermaidStyleTree);
+        }
+
+        [Fact]
+        public void single_token_deletion_output() {
+            // Setup
+            AntlrInputStream inputStream = AntlrInputStreamReader.Read("class T {{ int i; }");
+            GRun             grun        = GRun.Read(inputStream);
+            // Exercise
+            string output = grun.GetOutput();
+            // Verify
+            Approvals.Verify(output);
+        }
+
+    }
+
+}
