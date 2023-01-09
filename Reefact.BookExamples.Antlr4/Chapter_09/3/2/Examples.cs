eebe6d24e42572c7c85786beee0ad3ca4234fe24
Reefact <sylvain.aurat@reefact.net>
2023-01-09 08:55:53 +0100

    Add chapter 9.3.1. extaneous token examples (not in the book but interesting).

---
+++ Reefact.BookExamples.Antlr4/Chapter_09/3/2/Examples.cs
@@ -0,0 +1,43 @@
+﻿#region Usings declarations
+
+using Antlr4.Runtime;
+
+using ApprovalTests;
+using ApprovalTests.Reporters;
+
+using Reefact.BookExamples.Antlr4.Chapter_09._1;
+
+using Xunit;
+
+#endregion
+
+namespace Reefact.BookExamples.Antlr4.Chapter_09._3._2 {
+
+    [UseReporter(typeof(BeyondCompareReporter))]
+    public class Examples {
+
+        [Fact]
+        public void extraneous_input_9_lisp_style_tree() {
+            // Setup
+            AntlrInputStream inputStream = AntlrInputStreamReader.Read("class 9 T { int i; }");
+            GRun             grun        = GRun.Read(inputStream);
+            // Exercise
+            string output = grun.ToLispStyleTree();
+            // Verify
+            Approvals.Verify(output);
+        }
+
+        [Fact]
+        public void extraneous_input_9_mermaid_style_tree() {
+            // Setup
+            AntlrInputStream inputStream = AntlrInputStreamReader.Read("class 9 T { int i; }");
+            GRun             grun        = GRun.Read(inputStream);
+            // Exercise
+            string output = grun.ToMermaidStyleTree();
+            // Verify
+            Approvals.Verify(output);
+        }
+
+    }
+
+}
