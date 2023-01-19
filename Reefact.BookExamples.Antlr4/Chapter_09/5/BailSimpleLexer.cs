ccbe4da6ad1d4e7086bff983a88aac3a551d1114
Reefact <sylvain.aurat@reefact.net>
2023-01-19 07:27:09 +0100

    Add BailSimpleLexer.

---
+++ Reefact.BookExamples.Antlr4/Chapter_09/5/BailSimpleLexer.cs
@@ -0,0 +1,28 @@
+﻿#region Usings declarations
+
+using Antlr4.Runtime;
+
+#endregion
+
+namespace Reefact.BookExamples.Antlr4.Chapter_09._5 {
+
+    public sealed class BailSimpleLexer : SimpleLexer {
+
+        #region Constructors declarations
+
+        /// <inheritdoc />
+        public BailSimpleLexer(ICharStream input) : base(input) { }
+
+        /// <inheritdoc />
+        public BailSimpleLexer(ICharStream input, TextWriter output, TextWriter errorOutput) : base(input, output, errorOutput) { }
+
+        #endregion
+
+        /// <inheritdoc />
+        public override void Recover(LexerNoViableAltException e) {
+            throw new Exception(e.Message, e);
+        }
+
+    }
+
+}
